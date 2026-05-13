c8a958a2b5a72d5c4871b20cc7290205061168e0
Liang <william810212@gmail.com>
2026-05-13 17:02:30 +0800

    更新多個 HTML 文件中的 logo 顯示樣式，調整 CSS 樣式以提升響應式設計，並移除不必要的 CSS 類別以增強代碼整潔性。

--- src/js/index.js
+++ src/js/index.js
@@ -26,9 +26,6 @@ const menuIconClass = document.querySelector("#menuIcon").classList;
 const navbarClass = document.querySelector("#navbar").classList;
 const scrollButton = document.querySelector("#scrollButton");
 const about = document.querySelector("#about");
-const world = document.querySelector("#world");
-const worldTarget = document.querySelector("#worldTarget");
-
 window.addEventListener("scroll", function () {
   if (document.documentElement.scrollTop > 0) {
     header.classList.add("header-scrolled");
@@ -49,17 +46,6 @@ scrollButton.addEventListener("click", function () {
     behavior: "smooth",
     block: "start",
   });
-});
-
-world.addEventListener("click", function () {
-  worldTarget.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
-  bodyClass.remove("overflow-hidden");
-  headerClass.remove("header-active");
-  menuIconClass.remove("menuIcon-active");
-  navbarClass.remove("navbar-active");
 });
 
 // gotop
