bf553186069b2e05fd4e82213f54b702f9cf2332
Liang <william810212@gmail.com>
2026-05-13 23:01:36 +0800

    新增 `.card-lift` 類別以增強卡片的懸停效果，並在多個 CSS 文件中應用相關樣式，提升用戶互動體驗。

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,6 +64,7 @@ module.exports = {
         16: "0 0 16px 0 rgba(0, 0, 0, 0.16)",
         "8-16": "0 8px 16px rgba(0, 0, 0, 0.16)",
         "6-6": "6px 6px 0px rgba(218, 218, 218, 1)",
+        lift: "0 10px 16px 0 rgba(26, 37, 42, 0.12)",
       },
       dropShadow: {
         "8-8": "8px 8px 6px rgba(0, 0, 0, 0.48)",
